48f04da1f2bd135392ef6708847b14a39397fb9f
carlos <carlosthe19916@gmail.com>
2015-12-21 12:32:23 -0500

    refactor jefe caja

--- app/modules/usuario/controllers/buscar.client.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('usuario').controller('Usuario.BuscarUsuarioController',
-    function ($scope, $window, SGUsuarioKeycloak) {
-        $window.open(SGUsuarioKeycloak.$getCreateRealmUserUrl());
-    });
